acb627d5cbfdeb90a7033e644bf9cd9ad5dff082
Alena Gritsiuk <alena22292@mail.ru>
2020-11-19 16:50:57 +0100

    Create a cocktail app

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :cocktails, except: [:update, :edit, :destroy] do
+    resources :doses, only: [:new, :create]
+  end
+
+  resources :doses, only: :destroy
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
